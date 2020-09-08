5ab17ea2ff0a5bfb6934bdb7bbda21ed53a43a30
Opilane TTHK <opilane@tthk.ee>
2020-09-08 12:12:40 +0300

    Task1

---
+++ StringInter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName;
+            String LastName;
+            Console.WriteLine("What is your fist name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            //Console.WriteLine("Hello, " + FirstName + " " + LastName);
+            //string interpolation
+            //$ - alt + crtl+4 { - alt gr + 7, } - alt gr + 0
+            Console.WriteLine($"Hello, {FirstName} {LastName}");
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String FirstName;
+            String LastName;
+            int Age;
+            int Year;
+            String UserInput;
+
+            Console.WriteLine("What is your First Name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("When were you born?");
+            UserInput = Console.ReadLine();
+            Year = Int32.Parse(UserInput);
+            Age = 2020 - Year;
+
+            Console.WriteLine($"Greetings {FirstName} {LastName}, Age {Age}");
+        }
+    }
+}
